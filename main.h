b8c6caee9a3b0400cb994ca90dbf28df84d064b7
Yordanos Dereje <yordadereje@gmail.com>
2022-04-20 01:07:26 +0300

    0

--- main.h
+++ main.h
@@ -6,8 +6,11 @@
 #include <stdarg.h>
 
 int _printf(const char *format, ...);
+
 int write(int fd, const char *buf, int bytes);
+
 int _strlen(const char *s);
+
 int counter(const char *format);
 
 #endif
